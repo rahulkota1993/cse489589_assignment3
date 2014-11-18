21bd06648762dc256915e53b62772d16b7861f7c
rahulkota1993 <rahulkota1993@gmail.com>
2014-11-18 12:37:16 -0500

    topology complete

--- topology.c
+++ topology.c
@@ -9,14 +9,14 @@ struct idipport
  uint16_t topology_id;
  uint8_t topology_ip[4];
  uint16_t topology_port;
-};
+}a_idipport[5];
 
 struct ididcost
 {
  uint16_t myid;
  uint16_t neighbourid;
  uint16_t costlink;
-};
+}a_ididcost[5];
 
 int main (void)
 {
@@ -24,7 +24,7 @@ int main (void)
 	struct ididcost *topology_ididcost;
 	char buf[100];
 	char *check;
-	int i=1;
+	int i_topology=1;
 	uint16_t serverid,serverport;
 	uint8_t serverip[4];
 	uint16_t r_id,r_nid,r_cost;
@@ -32,6 +32,7 @@ int main (void)
 	int numofneighbours=0;
 	int lines=0;
 	int k;
+	int k_topology;
 	
 	int store[20][100];
 	FILE *p= fopen("topology.txt","r");
@@ -42,7 +43,7 @@ int main (void)
 		
   		while (fgets (buf, sizeof(buf), p))
   		{
-    		 	if(i==1)
+    		 	if(i_topology==1)
 		 	{
 				numofservers=atoi(buf);
 				printf("no. of servers=%d\n",numofservers);
@@ -50,7 +51,7 @@ int main (void)
 				
 
 			}
-			if(i==2)
+			if(i_topology==2)
 			{
 				numofneighbours=atoi(buf);
 				printf("no. of neighbours=%d\n",numofneighbours);
@@ -59,36 +60,55 @@ int main (void)
 				lines=numofservers+numofneighbours;
 				
 			}
-			//if((i>2) && (i<=(2+numofservers)))
-			if(i==3)		
+			if((i_topology>2) && (i_topology<=(2+numofservers)))
+			//if(i_topology==3)		
 			{
 				serverid=atoi(strtok(buf," "));
-				printf("server id =%d\n",serverid);
+				//printf("server id =%d\n",serverid);
 				check=strtok(NULL," ");
-				printf("check=%s\n",check);
+				//printf("check=%s\n",check);
 					
 				
 				serverport=atoi(strtok(NULL," "));
-				printf("server port =%d\n",serverport);
+				//printf("server port =%d\n",serverport);
 				getIP(serverip,check);
-				for(k=0;k<4;k++)
+				/*for(k=0;k<4;k++)
 				{
 				 printf("serverip[%d]=%d\n",k,serverip[k]);
+				}*/
+				
+				a_idipport[serverid].topology_id=serverid;
+				printf("id=%d\n",a_idipport[serverid].topology_id);
+				
+				 for(k_topology=0;k_topology<4;k_topology++)
+				{
+				 a_idipport[serverid].topology_ip[k_topology]=serverip[k_topology];
+				 printf("ip=%d\n",a_idipport[serverid].topology_ip[k_topology]);
 				}
+				a_idipport[serverid].topology_port=serverport;
+				printf("port=%d\n",a_idipport[serverid].topology_port);
 				
   	 	 	
   			}
-			if(i==8)
+			if(i_topology>(2+numofservers))
 			{
 				r_id=atoi(strtok(buf," "));
-				printf("r_id=%d\n",r_id);
+				//printf("r_id=%d\n",r_id);
 				r_nid=atoi(strtok(NULL," "));
-				printf("r_nid=%d\n",r_nid);
+				//printf("r_nid=%d\n",r_nid);
 				r_cost=atoi(strtok(NULL," "));
-				printf("r_cost=%d\n",r_cost);
+				//printf("r_cost=%d\n",r_cost);
+
+				a_ididcost[serverid].myid=r_id;
+				a_ididcost[serverid].neighbourid=r_nid;
+				a_ididcost[serverid].costlink=r_cost;
+				printf("id=%d,nid=%d,costlink=%d\n",a_ididcost[serverid].myid,a_ididcost[serverid].neighbourid,a_ididcost[serverid].costlink);
+
+				
+				
 			}
 				
-		i++;
+		i_topology++;
 		//printf("%s", buf);
  		}
 	}
